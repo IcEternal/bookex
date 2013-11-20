c6f25f23003283b82c084355c3965b2b50100f04
haichongfu2003 <121665841@qq.com>
2013-11-20 21:03:13 +0800

    Finished the fundamental admin for JCode.

--- application/controllers/jcode.php
+++ application/controllers/jcode.php
@@ -7,4 +7,42 @@ class Jcode extends CI_Controller {
 		$this->load->model('jcode_model');
 	}
 
+
+	function isAdmin(){
+		$adminList = array('JCode', 'haichongfu2003');
+		$username = $this->session->userdata('username');
+		return in_array($username, $adminList);
+	}
+	function admin(){
+		if (!$this->isAdmin()) {
+			echo '该账号不是JCode指定的账号。';
+			return;
+		}
+		$this->load->view('includes/header');
+		$this->load->view('jcode/admin');
+		$this->load->view('includes/footer');
+	}
+
+	function getTicketInfo(){
+		if (!$this->isAdmin()) {
+			echo '该账号不是JCode指定的账号。';
+			return;
+		}
+		$this->load->model('jcode_model');
+		$ticket_id = $this->input->get('ticket_id');
+		$result = $this->jcode_model->getTicketInfo($ticket_id);
+		if ($this->input->get('use') == 1) {
+			$this->jcode_model->useTicket($ticket_id);
+			$result = $this->jcode_model->getTicketInfo($ticket_id);
+		}
+		if ($result->num_rows() == 0) {$data = null; $valid = false;}
+		else {
+			$res = $result->result();
+			$data = $res[0];
+			$valid = true;
+		}
+		$this->load->view('includes/header');
+		$this->load->view('jcode/ticket_info', array('data' => $data, 'valid' => $valid));
+		$this->load->view('includes/footer');
+	}
 }
